d76318ea69869a312b251ffbbd3b2c1ab24087d4
ShettyPratik13 <patrickshetty@gmail.com>
2017-12-07 13:53:50 -0800

    Working on neural net...

---
+++ neural_net.py
@@ -0,0 +1,16 @@
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+# fix random seed for reproducibility
+seed = 1
+numpy.random.seed(seed)
+
